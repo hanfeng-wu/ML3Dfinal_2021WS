507a56f743c4647dd1d52268799f5c890638445a
Sven Lüpke <luepke@in.tum.de>
2022-01-14 00:33:24 +0100

    Adapted ShapeNetVox loader to full shapenet core

--- Data/ShapeNetDataLoader.py
+++ Data/ShapeNetDataLoader.py
@@ -1,9 +1,11 @@
 """PyTorch datasets for loading ShapeNet voxels and ShapeNet point clouds from disk"""
+from typing import Dict, List
 import torch
 from pathlib import Path
 import json
 import numpy as np
 import trimesh
+import csv
 
 from Data.binvox_rw import read_as_3d_array
 
@@ -13,21 +15,27 @@ class ShapeNetVox(torch.utils.data.Dataset):
     Dataset for loading ShapeNet Voxels from disk
     """
 
-    num_classes = 13  # we'll be performing a 13 class classification problem
-    dataset_path = Path("exercise_2/data/ShapeNetVox32")  # path to voxel data - make sure you've downloaded the ShapeNet voxel models to the correct path
-    class_name_mapping = json.loads(Path("exercise_2/data/shape_info.json").read_text())  # mapping for ShapeNet ids -> names
-    classes = sorted(class_name_mapping.keys())
-
-    def __init__(self, split):
+    def __init__(self, shapenet_core_path: Path, shapenet_splits_csv_path: Path, split: str):
         """
         :param split: one of 'train', 'val' or 'overfit' - for training, validation or overfitting split
         """
         super().__init__()
+        self._shapenet_core_path = shapenet_core_path
+        
+        # the format of model paths is f"{synsetId}/{modelId}"
+        self._model_paths: List[str] = self._load_model_paths(shapenet_splits_csv_path=shapenet_splits_csv_path, split=split)
+
+    def _load_model_paths(shapenet_splits_csv_path: Path, split: str) -> List[str]:
         assert split in ['train', 'val', 'overfit']
 
-        self.items = Path(f"exercise_2/data/splits/shapenet/{split}.txt").read_text().splitlines()  # keep track of shapes based on split
+        with open(str(shapenet_splits_csv_path), 'r') as read_obj:
+            csv_reader = csv.reader(read_obj)
+            # skip header
+            next(csv_reader)
+            return [f"{row[1]}/{row[3]}" for row in csv_reader if row[4] == split]
 
-    def __getitem__(self, index):
+
+    def __getitem__(self, index: int):
         """
         PyTorch requires you to provide a getitem implementation for your dataset.
         :param index: index of the dataset sample that will be returned
@@ -37,22 +45,19 @@ class ShapeNetVox(torch.utils.data.Dataset):
                  "label", a number in [0, 12] representing the class of the shape
         """
         # Get item associated with index, get class, load voxels with ShapeNetVox.get_shape_voxels
-        item = self.items[index]
-        # Hint: since shape names are in the format "<shape_class>/<shape_identifier>", the first part gives the class
-        item_class = item.split("/")[0]
-        # read voxels from binvox format on disk as 3d numpy arrays
-        voxels = ShapeNetVox.get_shape_voxels(item)
+        model_path: str = self._model_paths[index]
+        with open(self._shapenet_core_path / model_path / "model_normalized.solid.binvox", "rb") as fptr:
+            voxels = read_as_3d_array(fptr).astype(np.float32)
         return {
-            "name": item,
+            "name": model_path,
             "voxel": voxels[np.newaxis, :, :, :],  # we add an extra dimension as the channel axis, since pytorch 3d tensors are Batch x Channel x Depth x Height x Width
-            "label": ShapeNetVox.classes.index(item_class)  # label is 0 indexed position in sorted class list, e.g. 02691156 is label 0, 02828884 is label 1 and so on.
         }
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
         :return: length of the dataset
         """
-        return len(self.items)
+        return len(self._model_paths)
 
     @staticmethod
     def move_batch_to_device(batch, device):
@@ -61,18 +66,6 @@ class ShapeNetVox(torch.utils.data.Dataset):
         :return: None, modifies batch inplace
         """
         batch['voxel'] = batch['voxel'].to(device)
-        batch['label'] = batch['label'].to(device)
-
-    @staticmethod
-    def get_shape_voxels(shapenet_id):
-        """
-        Utility method for reading a ShapeNet voxel grid from disk, reads voxels from binvox format on disk as 3d numpy arrays
-        :param shapenet_id: Shape ID of the form <shape_class>/<shape_identifier>, e.g. 03001627/f913501826c588e89753496ba23f2183
-        :return: a numpy array representing the shape voxels
-        """
-        with open(ShapeNetVox.dataset_path / shapenet_id / "model.binvox", "rb") as fptr:
-            voxels = read_as_3d_array(fptr).astype(np.float32)
-        return voxels
 
 
 class ShapeNetPoints(torch.utils.data.Dataset):
