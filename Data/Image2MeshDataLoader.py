4780869889eebfca148eceaae1c70c1a5ec620ca
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2022-01-27 10:57:34 +0100

    updated image2mesh dataloader to read new dataset

--- Data/Image2MeshDataLoader.py
+++ Data/Image2MeshDataLoader.py
@@ -5,16 +5,16 @@ Dataloader for the image to mesh dataset
 import os
 import torch
 import numpy as np
-import scipy.io as sio
 import skimage.io as skio
 from skimage.transform import resize
 import trimesh
 import pandas as pd
 from pyntcloud import PyntCloud
+from Data.binvox_rw import read_as_3d_array
 
 class Image2MeshDataLoader():
 
-    def __init__(self,images_path = "Assets/Data/Image2Mesh/images/",meshes_path = "Assets/Data/Image2Mesh/meshes/",image_size= 256, voxel_dims = (32,32,32), sample_rate = 4096):
+    def __init__(self,images_path = "Assets/Data/Image2Mesh/train/images/",meshes_path = "Assets/Data/Image2Mesh/train/meshes/",image_size= 256, voxel_dims = (32,32,32), sample_rate = 4096):
         self.images_path = images_path
         self.meshes_path = meshes_path
         self.in_size =image_size
@@ -32,22 +32,23 @@ class Image2MeshDataLoader():
         available_images = os.listdir( self.images_path + "/"+ self.data[index] + "/")
         rand_indx = np.random.randint(0,len(available_images))
         image = self.images_path + "/"+ self.data[index] + "/" + available_images[rand_indx]
-        label = self.meshes_path + "/"+ self.data[index] + "/model.mat"
+        label = self.meshes_path + "/"+ self.data[index] + "/" + os.listdir( self.meshes_path + "/"+ self.data[index] + "/")[0]
         
         image = skio.imread(image)
         image = resize(image,(self.image_size, self.image_size))
-        mask = image[:,:,3]
-        mask = mask/np.max(mask)
-        image = image[:,:,:3]
-        image[mask!=1] = np.max(image)
         image = image/np.max(image)
         image = torch.from_numpy(np.array(image,dtype=np.float32))
         image = image.permute(2,0,1)
 
-        voxel = sio.loadmat(label)['input'].astype(np.uint8)[0]
-        mesh = trimesh.voxel.ops.matrix_to_marching_cubes(voxel)
-        label = self._point2vox_(mesh.sample(self.sample_rate),self.voxel_dims)
-        label = torch.from_numpy(label.astype(np.uint8))
+        with open(label, "rb") as fptr:
+            voxel = read_as_3d_array(fptr).astype(np.float32)
+        # voxel = sio.loadmat(label)['input'].astype(np.uint8)[0]
+        if(voxel.shape!=self.voxel_dims):
+            mesh = trimesh.voxel.ops.matrix_to_marching_cubes(voxel)
+            label = self._point2vox_(mesh.sample(self.sample_rate),self.voxel_dims)
+            label = torch.from_numpy(label.astype(np.uint8))
+        else:
+            label = torch.from_numpy(voxel.astype(np.uint8))
         return image.float(),label.float()
 
     def _point2vox_(self,points,dims=(32,32,32)):
