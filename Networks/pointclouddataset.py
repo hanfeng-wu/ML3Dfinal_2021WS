92bc26d08bab6b10adb529c7231a8c99b948f8d1
Sven Lüpke <luepke@in.tum.de>
2022-01-31 02:04:02 +0100

    Added pointcloud to voxel retrieval test

--- Networks/pointclouddataset.py
+++ Networks/pointclouddataset.py
@@ -136,4 +136,60 @@ class retrieval_dataset(torch.utils.data.Dataset):
     def get_gts(self):
         return self.gts    
 
+import csv
+
+class retrieval_dataset_voxel(torch.utils.data.Dataset):
+    def __init__(self, split, device='cuda'):
+        assert split in ['train', 'val', 'overfit']
+        self.device = device
+        datadir = './Data/shapenetdata/'
+        subdirs = []
+        pcds = []
+        gts = []
+        for x in os.walk(datadir):
+            subdirs.append(x[0])
+        subdirs = sorted(subdirs)[1:] 
+
+        
+        subdir_filter = []
+        with open("Data/shapenet_splits.csv", 'r') as read_obj:
+            csv_reader = csv.reader(read_obj)
+            # skip header
+            next(csv_reader)
+            for row in csv_reader:
+                if row[4] == split:
+                    subdir_filter.append(row[3])
+
+        
+        for subdir in subdirs:
+            include_sample = False
+            for dir in subdir_filter:
+                if dir in subdir:
+                    include_sample = True
+            if not include_sample:
+                continue
+
+            pcds.extend([{'partial': f'{subdir}/pcd_{index}.obj', 'full': f'{subdir}/pcd_gt.obj'}  for index in range(10)])
+            file_full = open(f'{subdir}/pcd_gt.obj', 'r')
+            obj_full = trimesh.exchange.obj.load_obj(file_full)
+            gts.append({'path': f'{subdir}/pcd_gt.obj', 'tensor': torch.tensor(obj_full['vertices']).float()})
+        self.gts = gts 
+        self.pcds = pcds       
+            
+    def __getitem__(self, index):
+        partial = self.pcds[index]['partial']
+        file_partial = open(partial, 'r')
+        obj_partial = trimesh.exchange.obj.load_obj(file_partial)
+        return {
+            'partial': torch.tensor(obj_partial['vertices']).float(),
+            'full': self.pcds[index]['full']
+        }
+    def __len__(self):
+        """
+        :return: length of the dataset
+        """
+        return len(self.pcds)        
+
+    def get_gts(self):
+        return self.gts    
     
